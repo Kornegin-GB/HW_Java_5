6e6b7811f5a8d92e857c324ab565e1f3074a2215
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-11-15 18:42:40 +0300

    Корректировка решения задачи 1

--- Task_1.java
+++ Task_1.java
@@ -27,12 +27,9 @@ public class Task_1 {
             if (map.containsKey(nums[i])) {
                 if (i - map.get(nums[i]) <= k) {
                     return true;
-                } else {
-                    map.put(nums[i], i);
                 }
-            } else {
-                map.put(nums[i], i);
             }
+            map.put(nums[i], i);
         }
         return false;
     }
